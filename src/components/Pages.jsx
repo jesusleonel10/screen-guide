d60b9dcff3e31f049994d9e4f5e9576f25cada53
unknown <jesusleot10@gmail.com>
2024-08-12 13:17:14 -0400

    Arreglado el como se ve la paginación en resoluciones pequeñas

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
 import { Pagination } from '@mui/material';
 import { createTheme, ThemeProvider  } from '@mui/material/styles';
 
 import './../scss/Pages.scss'
 
 const Pages = ({page, setPage, footer}) => {
+    const [widthScreen, setWidthScreen] = useState(window.innerWidth);
+
+    /* Al cargar el componente capturamos el ancho con window.innerWidth
+    usamos addeventlistener para capturar el cambio al ocurrir el resize 
+    usamos removeEventlistener para limpiar al quitar el componente
+    */
+    useEffect(() => {
+        const handleResize = () => {
+            setWidthScreen(window.innerWidth)
+        };
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        };
+    }, []);
+
     const theme = createTheme({
         palette: {
             aqua: {
@@ -22,14 +41,14 @@ const Pages = ({page, setPage, footer}) => {
         <ThemeProvider theme={theme}>
             <div className={`${footer ? 'pages pages-border' : 'pages'}`}>
                 <Pagination 
-                    // siblingCount={0}
+                    siblingCount={widthScreen < 768 ? 0 : 2}
                     count={50} 
                     page={page} 
                     color='aqua' 
                     shape='rounded' 
                     size='large' 
-                    showFirstButton
-                    showLastButton
+                    showFirstButton={widthScreen < 560 ? false : true}
+                    showLastButton={widthScreen < 560 ? false : true}
                     onChange={handleChange} />
             </div>
         </ThemeProvider>
