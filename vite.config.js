7f4480e8bae6c01ee12f403cf6857431bb6f0ec7
unknown <jesusleot10@gmail.com>
2024-08-12 22:36:30 -0400

    Configurado las variables de entorno

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "import.meta.env.VITE_API_KEY": import.meta.env.VITE_API_KEY,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
   }
 })
