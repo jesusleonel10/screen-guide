4c10550f91eda6782d8a88fc1fb199a991fdeb0b
unknown <jesusleot10@gmail.com>
2024-08-12 22:45:12 -0400

    Configurado las variables de entorno

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
 })
