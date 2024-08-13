09aeb8850af3d4c43d28d58990224fe02549088b
unknown <jesusleot10@gmail.com>
2024-08-12 21:50:51 -0400

    Configurado las variables de entorno

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -16,7 +16,7 @@ const useFetchData = (url, flag) => {
                 method: 'GET',
                 headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${process.env.API_KEY}`
+                Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
             }
             }
         try {
